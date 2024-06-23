a66ca5d72aa81f9cc8759b0c26f0023aaddb18c1
lucassneiva <lucassneiva.code@gmail.com>
2024-06-23 19:56:39 -0300

    feature: node/express task

---
+++ express-task/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use((req, res, next) => {
+  console.log(`Request Method: ${req.method}, Request URL: ${req.url}`);
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
